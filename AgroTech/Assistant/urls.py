4a304098ad05fdafaf45534ebaa1c05e8c72ff21
Purnima Katiyar <75112217+purnimakatiyar@users.noreply.github.com>
2023-04-08 17:36:49 +0530

    Update urls.py

--- AgroTech/Assistant/urls.py
+++ AgroTech/Assistant/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
     path('supplier/login/', views.supplier_login, name='supplier_login'),
+    path('farmer/login/', views.farmer_login, name='farmer_login'),
+    path('admin/login/', views.admin_login, name='admin_login'),
 ]
